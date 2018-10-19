e4747164ce24a97d3011ffab8859367a01a2d2e5
hegemonies <hegemonies@yandex.ru>
2018-10-19 20:52:06 +0700

    Fix 2lab/2task

--- 2lab/2task/task2.c
+++ 2lab/2task/task2.c
@@ -9,7 +9,6 @@
 #include <ctype.h>
 #include <string.h>
 
-// extern char **environ;
 static char **commands;
 static int count_commands;
 
@@ -28,9 +27,6 @@ int isCommand(char *str)
         return 0;
     }
 
-    // if (isalpha(str[0])) {
-    //     return 1;
-    // }
     for (int i = 0; i < count_commands; i++) {
         if (strcmp(commands[i], str) == 0) {
             return 1;
@@ -75,9 +71,6 @@ int isOption(char *str)
         return 0;
     }
 
-    // if (str[0] == '-') {
-    //     return 1;
-    // }
     if (!isCommand(str)) {
         return 1;
     }
@@ -131,20 +124,7 @@ int main(void)
     printf("pid = %d\ngid = %d\nsid = %d\n", (int)getpid(), (int)getgid(), (int)getsid(0));
 
     char *user = getenv("USER");
-    // char *user = calloc(1, sizeof(char));
-    // int i;
-    // for (i = 0; environ[13][i + 5] != '\0' && environ[13][i + 5] != '\n'; i++) {
-    //     user = realloc(user, sizeof(char) * (i + 2));
-    //     user[i] = environ[13][i + 5];
-    // }
-    // user[i] = '\0';
     char *pwd = getenv("PWD");
-    // char *pwd = calloc(1, sizeof(char));
-    // for (i = 0; environ[26][i + 4] != '\0' && environ[26][i + 4] != '\n'; i++) {
-    //     pwd = realloc(pwd, sizeof(char) * (i + 2));
-    //     pwd[i] = environ[26][i + 4];
-    // }
-    // pwd[i] = '\0';
 
     int Exit = 0;
     int count = 0;
@@ -153,17 +133,16 @@ int main(void)
         return 1;
     }
 
-    while (!Exit) {
+    while (1) {
         printf("%s:%s$: ", user, pwd);
         char *req = calloc(100, sizeof(char));
         if (!req) {
             return 1;
         }
         fgets(req, 100, stdin);
-        // printf("req = %s\n", req);
-        fflush(stdout);
-        // printf("\n");
-
+        if (!strcmp(req, "\0") || !strcmp(req, "\n")) {
+            break;
+        }
 
         for (int i = 0; req[i] != '\n' && req[i] != '\0'; count++, i++) {
             argv = realloc(argv, sizeof(char*) * (count + 1));
@@ -171,25 +150,25 @@ int main(void)
                 printf("Error malloc\n");
                 return 1;
             }
-            // printf("i = %d", i);
             char *tmp = calloc(50, sizeof(char));
             for (int j = 0; !isspace(req[i]) && req[i] != '\0' && req[i] != '\n'; j++, i++) {
-                // argv[count] = realloc(argv[count], sizeof(char) * (j + 1));
-                // if (!argv[count]) {
-                //     printf("Error malloc\n");
-                // }
-                // argv[count][j] = req[i];
                 tmp[j] = req[i];
+            }
+            printf("%s.\n", tmp);
+            if (tmp[0] == 0) {
+                printf("0\n");
+            }
+            if (strcmp(tmp, "exit") == 0) {
+                Exit = 1;
+                break;
             }
             argv[count] = calloc(getSize(tmp) + 1, sizeof(char));
             memcpy(argv[count], tmp, getSize(tmp) + 1);
         }
 
-        // free(req);
-
-        // for (int i = 0; i < count; i++) {
-        //     printf("%s\n", argv[i]);
-        // }
+        if (Exit) {
+            break;
+        }
 
         pid_t pid;
 
@@ -200,31 +179,21 @@ int main(void)
         
         if (pid == 0) {
             for (int i = 0; i < count; i++) {
-                if (strcmp(argv[i], "exit") == 0) {
-                    Exit = 1;
-                    break;
-                }
                 if (isCommand(argv[i])) {
-                    int *lcommand = calloc(getSize(argv[i]) + 6, sizeof(char));
-                    strcat(lcommand, "/bin/");
-                    strcat(lcommand, argv[i]);
-                    // printf("lcommand = %s\n", lcommand);
-                    // char *options = calloc(getSize(argv[i]), sizeof(char));
-                    // options = strcat(options, argv[i]);
+                    char *lcommand = calloc(getSize(argv[i]) + 6, sizeof(char));
+                    lcommand = strcat(lcommand, "/bin/");
+                    lcommand = strcat(lcommand, argv[i]);
                     char **options = calloc(1, sizeof(char*));
                     options[0] = calloc(getSize(argv[i]), sizeof(char));
                     i++;
                     for (int j = 1; isOption(argv[i]) || isOr(argv[i]) || isAnd(argv[i]); i++, j++) {
-                        // options = realloc(options, sizeof(char) * (getSize(options) + getSize(argv[i]) + 2));
-                        // options = strcat(options, " ");
-                        // options = strcat(options, argv[i]);
                         options = realloc(options, sizeof(char*) * (j + 1));
                         options[j] = calloc(getSize(argv[i]), sizeof(char));
                         memcpy(options[j], argv[i], getSize(argv[i]));
                     }
                     execv(lcommand, options);
-                    // free(lcommand);
-                    // free(options);
+                    free(lcommand);
+                    free(options);
                 } else {
                     if (isOr(argv[i]) || isAnd(argv[i])) {
                         i++;
@@ -249,6 +218,7 @@ int main(void)
             usleep(200000);
             int status;
             waitpid(pid, &status, WNOHANG);
+            printf("exitstatus = %d\n", WEXITSTATUS(status));
             // printf("status = %d\n", status);
             printf("%d status: %d\n", pid, status);
             if (status > 0) {
@@ -257,10 +227,10 @@ int main(void)
             // printf("status = %d\n", status);
         }
         count = 0;
-        // for (int i = 0; i < count; i++) {
-        //     free(argv[i]);
-        // }
-        // free(argv);
+        for (int i = 0; i < count; i++) {
+            free(argv[i]);
+        }
+        free(argv);
     }
 
 
